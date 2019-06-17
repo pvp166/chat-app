fcef4288a37e24fc59106b35e73aa7ec7ee231dc
pvp166 <31415913+pvp166@users.noreply.github.com>
2019-06-17 16:34:59 -0400

    Add files via upload

---
+++ src/components/MessageList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const DUMMY_DATA = [
+    {
+        senderID: blahblah
+        text: 
+    }
+];
+class MessageList extends React.Component{
+    
+    render(){
+        return (<div>
+
+        </div>)
+    }
+}
+
+export default MessageList;
